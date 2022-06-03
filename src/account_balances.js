c6cbf13673c633f1bc22feec29e9383ddabf915b
Nishit Chittora <nishitchittora2015@gmail.com>
2022-06-03 22:53:42 +0530

    account balance check added

--- src/account_balances.js
+++ src/account_balances.js
@@ -1,1 +1,11 @@
 const { ethers } = require("ethers");
+require("dotenv").config();
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_URL);
+
+const account_balance = async () => {
+  const balance = await provider.getBalance(process.env.ACCOUNT);
+  console.log(ethers.utils.formatEther(balance));
+};
+
+account_balance();
